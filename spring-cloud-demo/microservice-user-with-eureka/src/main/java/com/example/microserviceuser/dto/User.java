073a83610162fb121afe90e3a75ed173ea62a9dd
sunsiyuan <sun.siyuan@dr-elephant.com>
2018-11-21 17:31:33 +0800

    add gateway zuul

---
+++ spring-cloud-demo/microservice-user-with-eureka/src/main/java/com/example/microserviceuser/dto/User.java
@@ -0,0 +1,44 @@
+package com.example.microserviceuser.dto;
+
+public class User {
+
+    private Integer userId;
+
+    private String name;
+
+    private Integer age;
+
+    private String email;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
