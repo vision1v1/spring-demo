82f635bf8d9df9d3357c7c94d0fdb5f7feea3696
sunsiyuan <sun.siyuan@dr-elephant.com>
2018-11-21 15:54:19 +0800

    add microservice-user-with-eureka

---
+++ spring-cloud-demo/microservice-user-with-eureka/src/main/java/com/example/microserviceuser/MicroserviceUserWithEurekaApplication.java
@@ -0,0 +1,12 @@
+package com.example.microserviceuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceUserWithEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceUserWithEurekaApplication.class, args);
+    }
+}
---
+++ spring-cloud-demo/microservice-user-with-eureka/src/main/java/com/example/microserviceuser/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.example.microserviceuser.controller;
+
+import com.example.microserviceuser.dto.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(UserController.class);
+
+    @RequestMapping("/{userId}")
+    public User getUserById(@PathVariable("userId") Integer userId) {
+        LOGGER.trace("get user {}", userId);
+        LOGGER.debug("get user {}", userId);
+        LOGGER.info("get user {}", userId);
+        LOGGER.warn("get user {}", userId);
+        LOGGER.error("get user {}", userId);
+        User user = new User();
+        user.setUserId(userId);
+        user.setName("happy engineer");
+        user.setAge(100);
+        user.setEmail("test@123.com");
+        return user;
+    }
+
+}
