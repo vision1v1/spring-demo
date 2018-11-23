0f1077f3eb165a0cb18141a3f0cd24ba647513d2
sunsiyuan <sun.siyuan@dr-elephant.com>
2018-11-23 17:29:41 +0800

    switch LOGGER debug level

--- spring-cloud-demo/gateway-zuul-customer-route-locator/src/main/java/com/example/gatewayzuul/CustomRouteLocator.java
+++ spring-cloud-demo/gateway-zuul-customer-route-locator/src/main/java/com/example/gatewayzuul/CustomRouteLocator.java
@@ -3,6 +3,7 @@ package com.example.gatewayzuul;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.netflix.zuul.filters.RefreshableRouteLocator;
+import org.springframework.cloud.netflix.zuul.filters.Route;
 import org.springframework.cloud.netflix.zuul.filters.SimpleRouteLocator;
 import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
 
@@ -22,6 +23,15 @@ public class CustomRouteLocator extends SimpleRouteLocator implements Refreshabl
     }
 
     @Override
+    public Route getMatchingRoute(final String path) {
+        LOGGER.info("path : {}", path);
+        Route route = super.getMatchingRoute(path);
+        LOGGER.info("route : {}", route.toString());
+        return route;
+    }
+
+
+    @Override
     protected Map<String, ZuulProperties.ZuulRoute> locateRoutes() {
         LOGGER.info("{}", "-----------------------");
         Map<String, ZuulProperties.ZuulRoute> routeMap = new LinkedHashMap<String, ZuulProperties.ZuulRoute>();
@@ -30,13 +40,14 @@ public class CustomRouteLocator extends SimpleRouteLocator implements Refreshabl
         return routeMap;
     }
 
-    private Map<String, ZuulProperties.ZuulRoute> loadCustomRoute(){
+
+    private Map<String, ZuulProperties.ZuulRoute> loadCustomRoute() {
         Map<String, ZuulProperties.ZuulRoute> routeMap = new LinkedHashMap<String, ZuulProperties.ZuulRoute>();
         ZuulProperties.ZuulRoute route1 = new ZuulProperties.ZuulRoute();
         route1.setId("route1");
         route1.setPath("/thirdparty/**");
-        //route1.setUrl("http://localhost:9000");
-        route1.setServiceId("MICROSERVICE-USER-WITH-EUREKA");
+        route1.setUrl("http://localhost:9000");
+        //route1.setServiceId("MICROSERVICE-USER-WITH-EUREKA");
         //route1.setStripPrefix(true);
 
         ZuulProperties.ZuulRoute route2 = new ZuulProperties.ZuulRoute();
