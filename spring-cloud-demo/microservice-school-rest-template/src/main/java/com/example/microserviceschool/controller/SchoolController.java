420b9f6481664965e4eb286351ce2fb0b0d9c169
vision1v1 <vision1v1@163.com>
2018-11-28 15:51:14 +0800

    restTemplate usage

--- spring-cloud-demo/microservice-school-rest-template/src/main/java/com/example/microserviceschool/controller/SchoolController.java
+++ spring-cloud-demo/microservice-school-rest-template/src/main/java/com/example/microserviceschool/controller/SchoolController.java
@@ -1,13 +1,14 @@
 package com.example.microserviceschool.controller;
 
 import com.example.microserviceschool.sao.User;
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -18,12 +19,23 @@ public class SchoolController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private EurekaClient eurekaClient;
+
     @RequestMapping("/students")
     public ResponseEntity<List<User>> getStudents() {
-//      final ResponseEntity<User[]> result = restTemplate.getForEntity("http://MICROSERVICE-USER-PROVIDER/user/list", User[].class);
-        final ResponseEntity<User[]> result = restTemplate.getForEntity("http://localhost:9000/user/list", User[].class);
+        final ResponseEntity<User[]> result = restTemplate.getForEntity("http://MICROSERVICE-USER-PROVIDER/user/list", User[].class);
+        //final ResponseEntity<User[]> result = restTemplate.getForEntity("http://localhost:9000/user/list", User[].class);
+        //User[] users = restTemplate.getForObject("http://MICROSERVICE-USER-PROVIDER/user/list",User[].class);
         User[] users = result.getBody();
         return ResponseEntity.ok(Arrays.asList(users));
+    }
+
+    @RequestMapping("/test")
+    public ResponseEntity<String> test() {
+        InstanceInfo instance = eurekaClient.getNextServerFromEureka("MICROSERVICE-USER-PROVIDER", false);
+        String result = instance.getHomePageUrl() + " " + instance.getIPAddr() + ":" + instance.getPort();
+        return ResponseEntity.ok(result);
     }
 
 
