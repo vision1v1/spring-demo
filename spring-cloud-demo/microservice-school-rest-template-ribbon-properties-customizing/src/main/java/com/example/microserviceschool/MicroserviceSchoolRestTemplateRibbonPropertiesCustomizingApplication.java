bc8f180a57d6dddd9ae2bba324ac4751d6c1f04d
vision1v1 <vision1v1@163.com>
2018-12-01 21:23:18 +0800

    add ribbon-properties-customizing

---
+++ spring-cloud-demo/microservice-school-rest-template-ribbon-properties-customizing/src/main/java/com/example/microserviceschool/MicroserviceSchoolRestTemplateRibbonPropertiesCustomizingApplication.java
@@ -0,0 +1,26 @@
+package com.example.microserviceschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@Configuration
+public class MicroserviceSchoolRestTemplateRibbonPropertiesCustomizingApplication {
+
+    // @LoadBalanced 标记必须加上，否则restTemplate 不能自动根据Eureka查找服务实例
+    // issues https://github.com/spring-cloud/spring-cloud-netflix/issues/1119
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceSchoolRestTemplateRibbonPropertiesCustomizingApplication.class, args);
+    }
+}
---
+++ spring-cloud-demo/microservice-school-rest-template-ribbon-properties-customizing/src/main/java/com/example/microserviceschool/controller/SchoolController.java
@@ -0,0 +1,52 @@
+package com.example.microserviceschool.controller;
+
+import com.example.microserviceschool.sao.User;
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/school")
+public class SchoolController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SchoolController.class);
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
+    @RequestMapping("/students")
+    public ResponseEntity<List<User>> getStudents() {
+        final ResponseEntity<User[]> result = restTemplate.getForEntity("http://MICROSERVICE-USER-PROVIDER/user/list", User[].class);
+        //final ResponseEntity<User[]> result = restTemplate.getForEntity("http://localhost:9000/user/list", User[].class);
+        //User[] users = restTemplate.getForObject("http://MICROSERVICE-USER-PROVIDER/user/list",User[].class);
+        User[] users = result.getBody();
+        return ResponseEntity.ok(Arrays.asList(users));
+    }
+
+    @RequestMapping("/test")
+    public String test() {
+        ServiceInstance serviceInstance = loadBalancerClient.choose("MICROSERVICE-USER-PROVIDER");
+        LOGGER.info(" xxxxxxxxx serviceInstance : {} Ip : {} Port {}", serviceInstance.getInstanceId(), serviceInstance.getHost(), serviceInstance.getPort());
+
+        //ServiceInstance serviceInstance2 = loadBalancerClient.choose("MICROSERVICE-USER-PROVIDER2");
+        //LOGGER.info(" ooooooooo serviceInstance : {} Ip : {} Port {}", serviceInstance2.getInstanceId(), serviceInstance2.getHost(), serviceInstance2.getPort());
+
+        return "test ribbon Balancer Client";
+    }
+
+
+}
