4ec6f3efbd61e79ee06deb6b70a8584abdb3e195
sunsiyuan <sun.siyuan@dr-elephant.com>
2018-12-05 18:16:12 +0800

    add account-web-mysql-mybatis

---
+++ spring-boot-demo/account-web-mysql-mybatis/src/main/java/com/example/accountwebmysqlmybatis/AccountWebMysqlMybatisApplication.java
@@ -0,0 +1,12 @@
+package com.example.accountwebmysqlmybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountWebMysqlMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountWebMysqlMybatisApplication.class, args);
+    }
+}
