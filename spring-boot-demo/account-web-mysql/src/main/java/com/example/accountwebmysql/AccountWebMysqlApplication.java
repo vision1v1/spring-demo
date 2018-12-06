a9bfe5e905b39d618507a28453f768675a7562a2
sunsiyuan <sun.siyuan@dr-elephant.com>
2018-12-06 17:24:17 +0800

    add account-web-mysql empty

---
+++ spring-boot-demo/account-web-mysql/src/main/java/com/example/accountwebmysql/AccountWebMysqlApplication.java
@@ -0,0 +1,12 @@
+package com.example.accountwebmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountWebMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountWebMysqlApplication.class, args);
+    }
+}
