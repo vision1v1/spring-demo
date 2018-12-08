2df903791197584250fce0435ada4d262d2b1b2b
vision1v1 <vision1v1@163.com>
2018-12-08 12:45:19 +0800

    add account-web-mysql-jdbc

---
+++ spring-boot-demo/account-web-mysql-jdbc/src/main/java/com/example/accountwebmysql/AccountWebMysqlJdbcApplication.java
@@ -0,0 +1,12 @@
+package com.example.accountwebmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountWebMysqlJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountWebMysqlJdbcApplication.class, args);
+    }
+}
