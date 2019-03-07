3a951f50285da178be39e32ccc63b0b93c97b9dd
vision1v1 <vision1v1@163.com>
2019-03-07 23:41:45 +0800

    just debugging

---
+++ spring-boot-demo/spring-boot-debug/src/main/java/com/example/springbootdebug/SpringBootDebugApplication.java
@@ -0,0 +1,16 @@
+package com.example.springbootdebug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringBootDebugApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDebugApplication.class, args);
+
+
+    }
+
+}
