51d6511e52e7dc9422b1d9f77e7ed22bc2ae0333
vision1v1 <vision1v1@163.com>
2019-03-07 23:36:16 +0800

    test autowired for getting ApplicationContext

--- spring-boot-demo/spring-boot-application-context-aware/src/main/java/com/example/springbootapplicationcontextaware/BeanController.java
+++ spring-boot-demo/spring-boot-application-context-aware/src/main/java/com/example/springbootapplicationcontextaware/BeanController.java
@@ -1,13 +1,16 @@
 package com.example.springbootapplicationcontextaware;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController()
 @RequestMapping("bean")
@@ -15,6 +18,9 @@ public class BeanController {
 
     @Autowired
     private BeanProvider beanProvider;
+
+    @Autowired
+    ApplicationContext context;
 
     @RequestMapping(path = "/names",method = RequestMethod.GET)
     public ResponseEntity<List<String>> getAllBeanNames(){
@@ -25,6 +31,11 @@ public class BeanController {
     @GetMapping(path = "/controllers")
     public ResponseEntity<List<String>> getAllControllersBeanNames(){
         return ResponseEntity.ok(beanProvider.getAllControllerBeanNames());
+    }
+
+    @RequestMapping(path = "/v2/names",method = RequestMethod.GET)
+    public ResponseEntity<List<String>> getAllBeanNamesV2(){
+        return ResponseEntity.ok(Arrays.stream(context.getBeanDefinitionNames()).sorted().collect(Collectors.toList()));
     }
 
 }
